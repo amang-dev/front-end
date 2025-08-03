321653f30bdc7f96c24cfb167dfc0a0ec8ea9897
Aman Gupta <amangupta99344@gmail.com>
2025-08-03 22:38:50 +0530

    password generator

---
+++ JavaScript/Js Rivison/leetcode.js
@@ -0,0 +1,71 @@
+// function calc(val){
+
+// return {
+//         add(val1){
+//             var a = val + val1
+//             return a
+//         },
+
+//         sub(val1){
+//             var b = val - val1
+//             return b
+//         }
+//     }
+
+//     // return obj
+// }
+
+// console.log(calc(5).add(5));                        
+// console.log(calc(7).sub(3));  
+
+
+// function calc(val){
+
+
+//      function   add(val1){
+//             var a = val + val1
+//             return a
+//         }
+
+//        function sub(val1){
+//             var b = val - val1
+//             return b
+//         }
+
+//         return obj ={
+//             add, sub
+//         }
+
+//     }
+
+    
+// console.log(calc(5).add(5));                        
+// console.log(calc(7).sub(3));  
+
+
+    var createCounter = function(init) {
+
+    return obj = {
+        increment(init){
+          var a  = init++
+          return a 
+        },
+    
+        decrement(init){
+          var b  = --init
+          return b 
+        },
+
+        reset(init){
+          var c = init
+          return c
+        }
+
+    }
+};
+
+
+const counter = createCounter()
+console.log(counter.increment(5));
+console.log(counter.reset(5));
+console.log(counter.decrement(5));
